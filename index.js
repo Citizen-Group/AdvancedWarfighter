f1952e44f4c290ca02fd06ac8b69d0e62a034717
Justin Kelly <Blue_Lt@hotmail.com>
2016-08-28 12:15:36 -0400

    Add new response structure + cross access
    Added in CORS and a new response structure for starting work on AJAX.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
-app.get('/', function (req, res){
-res.send('Hello World!');
-});
+var app = express()
 
-app.get('/dick', function (req, res){
-res.send('For Harbre!');
-});
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+  // CORS for DEV
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  console.log(req.body); // populated!
+  response.send(request.body);    // echo the result back
+  next();
+})
 
 app.listen(3000,function() {
 console.log('example app listening on port 3000!');
